c16360419ec3632d00c14dd59af1727bd3792b90
Dovi Lilling <dovi_lilling@epam.com>
2020-08-09 20:23:00 +0300

    Dovi HW 2020/08/09

--- src/main/java/doviHW/com/hw20200809/MultithreadingMain.java
+++ src/main/java/doviHW/com/hw20200809/MultithreadingMain.java
@@ -6,7 +6,7 @@ import java.util.List;
 public class MultithreadingMain {
     public static void main(String[] args) {
         List<Thread> threadsList = new ArrayList<>();
-        for (int i=0; i < 10; i++){
+        for (int i=0; i < 200; i++){
             threadsList.add(new Thread(new MyCounter(i), "counter " + i));
             threadsList.get(i).start();
         }
